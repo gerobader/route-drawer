32847504460b8f322d2abca1b0c98fd6e41e0188
Gero Bader <gero.bader@web.de>
2022-04-29 13:22:44 +0200

    server port adjust

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.get('/routes', (req, res) => {
   res.send(routes);
 });
 
-app.listen(3000, () => {
-  console.log('Server has started on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server has started on port', process.env.PORT || 3000);
 });
